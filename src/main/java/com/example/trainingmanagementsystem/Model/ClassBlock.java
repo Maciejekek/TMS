afbd4c1ce89a2cfc80a113b87e4e903ac6eb0030
Maciejekek <95121937+Maciejekek@users.noreply.github.com>
2022-06-12 10:24:08 +0200

    basic filds in models

--- src/main/java/com/example/trainingmanagementsystem/Model/ClassBlock.java
+++ src/main/java/com/example/trainingmanagementsystem/Model/ClassBlock.java
@@ -1,4 +1,22 @@
 package com.example.trainingmanagementsystem.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassBlock {
+    Long id;
+
+    String name;
+
+    List<Classes> classesList;
+
+    List<User> userList;
 }
--- src/main/java/com/example/trainingmanagementsystem/Model/User.java
+++ src/main/java/com/example/trainingmanagementsystem/Model/User.java
@@ -1,4 +1,33 @@
 package com.example.trainingmanagementsystem.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    Long ID;
+
+    String login;
+
+    String password;
+
+    String type;
+
+    String name;
+
+    String lastName;
+
+    Boolean isActive;
+
+    List<Course> courseList;
+
+    List<UserNotification> notifications;
 }
