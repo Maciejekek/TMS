d33727bfe9dcff8ba66af11dc9977ae6b2681778
MrMag1990 <MrMag1990@kursSDA.pl>
2022-06-12 10:30:26 +0200

    feature/TMS-create_spring_model create contr,repo,serv

--- src/main/java/com/example/trainingmanagementsystem/repository/ClassesRepository.java
+++ src/main/java/com/example/trainingmanagementsystem/repository/ClassesRepository.java
@@ -3,5 +3,5 @@ package com.example.trainingmanagementsystem.repository;
 import com.example.trainingmanagementsystem.Model.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassesController extends JpaRepository<Classes, Long> {
+public interface ClassesRepository extends JpaRepository<Classes, Long> {
 }
