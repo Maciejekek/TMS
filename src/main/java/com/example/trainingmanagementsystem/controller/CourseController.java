07a37610a983a44b857d6c52c145e6125350ec57
Maciejekek <95121937+Maciejekek@users.noreply.github.com>
2022-06-25 14:42:41 +0200

    is thad right?

--- src/main/java/com/example/trainingmanagementsystem/controller/CourseController.java
+++ src/main/java/com/example/trainingmanagementsystem/controller/CourseController.java
@@ -1,6 +1,13 @@
 package com.example.trainingmanagementsystem.controller;
 
+import com.example.trainingmanagementsystem.Model.ClassBlock;
+import com.example.trainingmanagementsystem.Model.Course;
+import com.example.trainingmanagementsystem.service.CourseService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //todo dodawanie usuwanie edycja kursu(admin)
 
@@ -8,5 +15,30 @@ import org.springframework.stereotype.Controller;
 
 //todo dodawanie/usuwanie/edytowanie listy course
 
+@RestController
 public class CourseController {
+
+    public CourseController(CourseService service) {
+        this.service = service;
+    }
+
+    CourseService service;
+
+    @GetMapping("/course")
+    public List<Course> getAllCourse(){
+        return service.findAll();
+    }
+
+    @GetMapping("/course/person={id}")
+    public List<Course> getCourseByPersonId(@RequestBody Long id){
+        return service.getCourseByPersonId(id);
+    }
+
+    @PutMapping("/course/addclassblock?course={id}")
+    public ResponseEntity<Course> addBlock(@PathVariable Long id, @RequestBody ClassBlock classBlock){
+        return service.addBlockInToCourse(id, classBlock);
+    }
+
+
+
 }
