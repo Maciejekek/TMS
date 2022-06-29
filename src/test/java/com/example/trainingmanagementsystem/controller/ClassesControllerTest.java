1f583bbfe3f0b9946c91a4cf7a6951649434b428
MrMag1990 <MrMag1990@kursSDA.pl>
2022-06-29 11:00:24 +0200

    TMS-14_Controller_Classes_Testing - don`t merge !!

--- src/test/java/com/example/trainingmanagementsystem/controller/ClassesControllerTest.java
+++ src/test/java/com/example/trainingmanagementsystem/controller/ClassesControllerTest.java
@@ -1,39 +1,70 @@
 package com.example.trainingmanagementsystem.controller;
 
-import com.example.trainingmanagementsystem.Model.Classes;
-import com.example.trainingmanagementsystem.service.ClassesService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Date;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-class ClassesControllerTest {
+@AutoConfigureMockMvc
+@SpringBootTest
+class ClassBlockControllerTest {
 
-    private final static Classes CLASSES = new Classes(1L, "topic", new Date());
-
-    @Mock
-    private ClassesService classesService;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void getAllClasses() {
-        classesService.findAll();
+    @DisplayName("Should Create MockMvc")
+    void shouldCreateMockMvc() {
+        assertNotNull(mockMvc);
     }
 
     @Test
-    void postAddClasses() {
-        classesService.getAddClass(CLASSES);
+    @DisplayName("Should get all class blocks")
+    void shouldGetAllClassBlocks() throws Exception {
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/classBlocks"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Matchers.is(1)))
+                .andExpect(jsonPath("$[0].id").exists())
+                .andExpect(jsonPath("$[0].name").exists());
+    }
+
+    @Test
+    @DisplayName("Should get class block by id ")
+    void shouldPostClassBlockById() throws Exception {
+
+        this.mockMvc
+                .perform(post("/classBlocks/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\" : \"name\"}"))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("name"));
+
     }
 
     @Test
     void patchClasses() {
-        classesService.getEditClass(CLASSES);
+
+
     }
 
     @Test
     void deleteClasses() {
-        classesService.deleteClassById(CLASSES.getId());
+
     }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into class_block(id, class_block_name)
+values (1, 'test');
+
+insert into class_block(id, class_block_name)
+values ('test');
+
