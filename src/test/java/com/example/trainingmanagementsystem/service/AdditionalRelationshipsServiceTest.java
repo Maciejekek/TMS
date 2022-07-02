757b4820c3c9aee32ba93b8b074b34f06064a1e2
MrMag1990 <MrMag1990@kursSDA.pl>
2022-07-02 09:59:09 +0200

    TMS-14_Controller_Classes_Testing

--- src/test/java/com/example/trainingmanagementsystem/service/AdditionalRelationshipsServiceTest.java
+++
@@ -1,5 +0,0 @@
-package com.example.trainingmanagementsystem.service;
-
-class AdditionalRelationshipsServiceTest {
-
-}
--- src/test/java/com/example/trainingmanagementsystem/service/ParticipantApplicationServiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.trainingmanagementsystem.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ParticipantApplicationServiceTest {
-
-}
