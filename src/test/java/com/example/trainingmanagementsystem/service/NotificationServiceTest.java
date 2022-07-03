0ae7892daf67775714ae7fdc12332df33b1506f1
MrMag1990 <MrMag1990@kursSDA.pl>
2022-07-03 11:31:54 +0200

    add all tests service layer

---
+++ src/test/java/com/example/trainingmanagementsystem/service/NotificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.trainingmanagementsystem.service;
+
+import org.junit.jupiter.api.Test;
+
+class NotificationServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/java/com/example/trainingmanagementsystem/service/PersonServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.trainingmanagementsystem.service;
+
+import org.junit.jupiter.api.Test;
+
+class PersonServiceTest {
+
+    @Test
+    void findAllPersons() {
+    }
+
+    @Test
+    void findAllPersonsAccountData() {
+    }
+
+    @Test
+    void savePerson() {
+    }
+
+    @Test
+    void savePersonAccountData() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findPersonDataById() {
+    }
+
+    @Test
+    void editPersonAccountData() {
+    }
+
+    @Test
+    void editPerson() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+}
